324bf10b7734c55da5d3c8bf99e679b273a91929
Ahlam-Nasser <ahlam.almadhlaee@gmail.com>
2023-08-11 10:15:09 -0700

    c code

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *  main - entry point
+ *  Description: print numbers 0-9
+ *  Return: 0 (success)
+ */
+int main(void)
+{
+	int digit = 0;
+
+	while (digit <= 9)
+	{
+		putchar(digit + '0');
+		digit++;
+	}
+	putchar('\n');
+
+	return (0);
+}
