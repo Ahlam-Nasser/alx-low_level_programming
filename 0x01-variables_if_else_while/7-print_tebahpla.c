aea7372167d2cce7a4944b642ae8afac09132f66
Ahlam-Nasser <ahlam.almadhlaee@gmail.com>
2023-08-11 10:25:27 -0700

    c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ *  main - entry point
+ *  Description: print all alphabet letters in reverse
+ *  Return: 0 (success)
+ */
+int main(void)
+{
+
+	char ch = 'z';
+
+
+	while (ch >= 'a')
+
+	{
+
+		putchar(ch);
+
+		ch--;
+
+	}
+
+	putchar('\n');
+
+	return (0);
+}
