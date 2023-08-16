68d7a61c0ccd0b139fb18a5339a06568eb59baa5
Ahlam-Nasser <ahlam.almadhlaee@gmail.com>
2023-08-15 17:28:24 -0700

    coding

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,8 @@
 #include "main.h"
+
 /**
+ * main - entry point
  * _putchar - writes the character c to stdout
- * @c: The character to print
  *
  * Return: On success 1.
  *         On error, -1 is returned, and error is set appropriately
