c8e4dd18652531fbaf79f45877c36e6861e7b657
Ahlam-Nasser <ahlam.almadhlaee@gmail.com>
2023-08-17 10:37:56 -0700

    more loops

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,13 +7,13 @@
  */
 void print_line(int n)
 {
-	int lnchr;
+	int lnChr;
 
 	if (n <= 0)
 		_putchar('\n');
 	else
 	{
-		for (lnchr = 1; lnchr <= n; lnchar++)
+		for (lnChr = 1; lnChr <= n; lnChr++)
 			_putchar('_');
 		_putchar('\n');
 	}
