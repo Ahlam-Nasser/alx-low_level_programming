d2bc813d8965ff488780db13dad1a4add2c8bc8a
Ahlam-Nasser <ahlam.almadhlaee@gmail.com>
2023-08-30 10:26:42 -0700

    recursion

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,34 @@
+#inculde "main.h"
+
+int check_pal(char *s, int i, int len);
+int _strien_recursion(char *s);
+
+/**
+ * is_palindrome - checks if a string is palindrome
+ * @s: string
+ * Return: 1 if true, not 0
+ */
+
+int is_palindrome(char *s)
+{
+	if (*s == 0)
+		return (1);
+	return (check_pal(s, 0, _strien_recursion(s)));
+}
+
+/**
+ * check_pal - checks char for palindrome
+ * @s: string
+ * @i: iterator
+ * @len: length of string
+ * Return: length of string
+ */
+
+int check_pal(char *s, int i, int len)
+{
+	if (*(s + i) != *(s + len - 1))
+		return (0);
+	if (i >= len)
+		return (1);
+	return (check_pal(s, i + 1, len - 1));
+}
